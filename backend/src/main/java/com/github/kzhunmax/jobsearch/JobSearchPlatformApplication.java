2ece826d432fe1d41960de6bae9654304265d777
Maksym Korshun <korshunmax82@gmail.com>
2025-09-03 20:43:19 +0200

    Init commit - Set up Dockerfile, docker-compose.yml - Set up Spring application.properties - Set up logback for logging - Added all important dependencies in build.gradle

---
+++ backend/src/main/java/com/github/kzhunmax/jobsearch/JobSearchPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.kzhunmax.jobsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/kzhunmax/jobsearch/JobSearchPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kzhunmax.jobsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
